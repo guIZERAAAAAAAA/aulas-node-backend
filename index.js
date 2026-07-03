fd9221ca7adb255a19a7595b5916810babec8eb2
“guilherme” <guilhermedamaia64@gmail.com>
2026-07-02 22:05:39 -0300

    iniciando a api com o node e express

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-import express from "express"
-                                                                       //server ou app 
+import express from 'express'
+
+// server ou app
 const app = express()
-               
-// configura uma rota para o metodo HTTP GET na raiz ("/") do aplicativo 
+//- path params - router
+app.get("/api/v1/somar", (req, res) => {
+    // query params
+    console.log(req.query)
 
+    const num1 = Number(req.query.num1)
+    const num2 = Number(req.query.num2)
+    const resultado = num1 + num2
 
-app.get("/api/v1/somar", (req, res ) => {
+    res.send({ resultado, "Tarefa": "Pesquise sobre QueryParams" })
+})
 
-console.log({ })
+// aqui em diante
 
-const num1 = Number(req.query.num1)
-const num2 = Number(req.query.num2)
-const resultado = num1 + num2
-                                                                       // retorna uma resposta JSON contendo uma mensagem "Hello world"
-    res.send({  resultado  })
-} )
-                                                                       // inicia o servidor express na porta 3000 e define uma função de retorno de chamada
+// metodo GET da função de subtrair usando QueryParam
+// Com numero1 e numero2 ou num1 e num2
+
 app.listen(3000, () => {
-                                                                       // exibe uma mensagem no console quando o servidor estiver ativo 
-    console.log("Servidor ouvindo na porta 3000 ")
-} )
-
-//////////////////////////////////////////desafio ////////////////////////////////////////////////////////////
-
-
-
-
-
+    console.log("Servidor ouvindo na porta 3000")
+})
