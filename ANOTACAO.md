138c8e7b2d20461fef0e99209f3c335767e0639a
“guilherme” <guilhermedamaia64@gmail.com>
2026-07-08 21:01:10 -0300

    contas e appredizado do node e json

---
+++ ANOTACAO.md
@@ -0,0 +1,90 @@
+Branch- nomenclatura
+Não pode haver 2 branchs com o mesmo nome
+Feat- criação de algo novo no projeto
+Fix-Correção ( nome da coisa ) não escreva muito ( so o básico )
+Doc – documentação
+Telinha de pull request
+/////////////////////////////////////////////////
+2/7/26 2 aula
+Brach
+Nunca devemos trabalhar na main principal , devemos trabalhar na Branch DEV
+( PROFICIONALMENTE )
+Parte do BACK-END
+Logica de negócios
+Banco de dados
+Segurança
+Escalabilidade
+NPM .com
+///////////////////////////////////////////////////
+6/7/26 3 aula
+ 
+Para atualizar sempre use o comando
+coloque o “dev” = : “node —watch index.js”
+Array = é um espaço na memoria 
+o array começa com zero
+
+COMANDOS
+
+find : encontrar
+
+get : pegar
+ ( UTILIZADA PARA RECUPERAR UMA LISTA OU UM UNICO RECURSO )
+
+put : meio que atualizar 
+( UTILIZADA PARA ATUALIZAR UM RECURSO )
+
+push : pucha / chama
+req : requerir
+query : parametro de consulta
+res : resposta
+it : coisa
+send : mandar
+null : nada
+status :
+
+/////////////////////////////----------------------------------->
+8/7/26
+
+ANOTACAO 
+
+AULA 4
+
+require VS Import 
+
+require 
+
+é o sistema de modulos nativos de node.js. ele existe desde o começo de do node é é chamado CommonJS
+
+( muda o jeito de importar ) ---const app = express()--- 
+
+não exporte defaul VOCE exporta uma COiSA SÒ
+
+ http 
+
+ a comunicaçao 
+
+ status ( pesquisar ) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,63 +1,49 @@
 import express from 'express'
-import bancoDeDados from './repository/index.js'
 
 // server ou app
 const app = express()
-//- path params - router
 
-app.put("/api/pessoa/:id", (req, res) => {
-    const id = req.params.id
+//somar 
+app.get("/api/v1/somar", (req, res) => {
 
- const pessoa = bancoDeDados.find(it => it.id == id ) 
-
-if (!pessoa){
-    res.send({ message : "Pessoas não encontrada "})
-    return
-} 
-
-   res.send({pessoa})
-
-})
-
-app.put("/api/pessoa/",(req,res) => {
-//const name = req.query.id
-//const name = req.query.name pode ser assim mais o debaixo é melhor 
- const { id , name } = req.query
-
-//if(id == || name == null ) { ( pode ser assim mas tem outro jeito )
-if( !id || !name ) {
-    res.send({message: " Favor informar id e name " })
-    return
-}
-
-
-bancoDeDados.push({id, name })
-res.send({message: "pessoa criada com sucesso " })
+    const { num1, num2 } = req.query
+    const resultado = Number(num1) + Number(num2)
+//validações quanto ao numero informado 
+    res.status (200).send({message : resultado })
+    //    res.send (200).send({message : resultado})
 })
 
 
-////////////////////////////////////////////////////////////////////
+//subtrair 
+
+app.get("/api/v1/subtrair", (req,res ) => {
+    const { num1, num2 } = req.query 
+    const resultado = Number(num1) - Number(num2)
+    res.status (200).send ({message : resultado })
+})
+
+
+//mult
+
+
+app.get ("/api/v1/multiplicasao" ,(req,res) => {
+const { num1, num2 } = req.query
+const resultado = Number(num1) * (num2) 
+res.status (200).send ({message : resultado })})  
 
 
 
 
+//div 
 
 
-
-
-
-
-
-
-
-
-
-
+app.get ("/api/v1/divisao" ,(req,res) => {
+const { num1, num2 } = req.query
+const resultado = Number(num1) / (num2) 
+res.status (200).send ({message : resultado })})  
 
 
 app.listen(3000, () => {
     console.log("Servidor escutando na porta 3000")
 })
-
-    ////////////////////////////////////////////////////////
 
